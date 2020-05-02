fb5867ef312de0cada403fbb27485f5dd3353e88
Will Newell <newellwm@gmail.com>
2020-05-01 20:55:47 -0400

    Fix entity .toStrings and missing app annotations

--- src/main/java/com/revolver/entities/User.java
+++ src/main/java/com/revolver/entities/User.java
@@ -89,7 +89,7 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", tickets=" + tickets.toString()
-				+ ", role=" + role.toString() + "]";
+				+ ", role=" + role.getTitle() + "]";
 	}
 	
 	
