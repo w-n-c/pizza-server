6b4e5b5febaa072e3f374218703d63d64058046b
unknown <macchasherpa@gmail.com>
2020-05-05 07:56:43 -0400

    added topping service

---
+++ src/main/java/com/revolver/services/ToppingService.java
@@ -0,0 +1,11 @@
+package com.revolver.services;
+
+import java.util.List;
+import java.util.Set;
+
+import com.revolver.entities.Topping;
+
+public interface ToppingService {
+
+	List<Topping> getAllToppings();
+}
